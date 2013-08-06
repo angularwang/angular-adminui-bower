0d76ca4d6f5e9ddcecb98396516f9baff94fbfad
Fengming Sun <s@sfmblog.cn>
2013-08-06 23:36:20 +0800

    add isFlash in flash service

--- scripts/angular-adminui.js
+++ scripts/angular-adminui.js
@@ -1,8 +1,12 @@
 (function () {
   function flashService($rootScope) {
     return {
-      notify: function (message) {
-        $rootScope.$emit('event:notification', message);
+      notify: function (message, isFlash) {
+        if (isFlash) {
+          $rootScope.$emit('event:flashMessageEvent', message);
+        } else {
+          $rootScope.$emit('event:notification', message);
+        }
       }
     };
   }
